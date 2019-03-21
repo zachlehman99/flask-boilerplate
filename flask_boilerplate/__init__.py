75f6951b4f8f1eeb6120f1e888b1869d51500838
Zach Lehman <zalehman182@stevenscollege.edu>
2019-03-21 19:22:41 -0400

    Create new routes and html fo those routes
    - add css to pages
    - create html pages for hello, index, calculate routes
    - create base layout with base and footer html

--- flask_boilerplate/__init__.py
+++ flask_boilerplate/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, render_template
 
 
 def create_app(test_config=None):
@@ -16,24 +16,75 @@ def create_app(test_config=None):
     @app.route('/')
     def index():
         # print('=' * 20)
-        # print(request.date)
+        # print(request.)
         # print('=' * 20)
-        return 'This is a flask-boilerplate project, not to be used in production'
+        return render_template('index.html')
 
     @app.route('/hello')
     def hello():
 
-        for key, value in request.args.items():
-            print(f"{key}: {value}")
+        # for key, value in request.args.items():
+        #     print(f"{key}: {value}")
 
         # print(request.args)
         # print(type(request.args))
         name = request.args.get('name', 'World')
-        return f"Hello {name}!"
+        return render_template('hello.html', name=name)
+
+    method_route_allows = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+
+    @app.route('/method', methods=method_route_allows)
+    def method_route():
+        return render_template('method.html', allowed=method_route_allows, method=request.method)
+
+
+    @app.route('/calculate', methods=['GET', 'POST'])
+    def calculate():
+        if request.method == 'GET':
+            return render_template('calculate.html', action='Add')
+        elif request.method == 'POST':
+            x = float(request.form['x'])
+            y = float(request.form['y'])
+            action = request.form['action']
+
+            if action == 'Add':
+                result = x + y
+            elif action == 'Subtract':
+                result = x - y
+            elif action == 'Multiply':
+                result = x * y
+            elif action == 'Divide':
+                result = x / y
+
+        return render_template('calculate.html', result=result, x=x, y=y)
 
     @app.route('/number/<int:num>')
     def number_route(num):
         return f"Number: {num}"
+
+    # @app.route('/method', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+    # def method_route():
+    #     if request.method == 'GET':
+    #         return f"HTTP Method: GET"
+    #     elif request.method == 'POST':
+    #         return f"HTTP Method: POST"
+    #     elif request.method == 'PATCH':
+    #         return f"HTTP Method: PATCH"
+    #     elif request.method == 'PUT':
+    #         return f"HTTP Method: PUT"
+    #     elif request.method == 'DELETE':
+    #         return f"HTTP Method: DELETE"
+    #     else:
+    #         return 'You done did break it'
+
+    @app.route('/status')
+    def status_route():
+        code = request.args.get('c', 200)
+        response = make_response(f"status: {code}", code)
+
+        return response
+
+
 
     @app.route('/mystuff')
     def mystuff():
